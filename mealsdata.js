4f61598f4dbff77364360514e9a272731cba7db3
shan-akbar-brainx <shan.akbar@brainxtech.com>
2021-09-07 19:08:56 +0500

    Created meals page and card for adding meals

---
+++ mealsdata.js
@@ -0,0 +1,253 @@
+const meals = [{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+},{
+    "img":"meal-2.jpg",
+    "title":"Homestyle Chicken",
+    "desc":"with Masterful Mac & Cheese",
+    "other_desc":"Gluten Free",
+    "cals":560,
+    "carb":35
+},{
+    "img":"meal-3.jpg",
+    "title":"Cauliflower Shell Beef Bolognese",
+    "desc":"with Nonna’s Soffritto & Italian Cheeses",
+    "other_desc":"Gluten Free",
+    "cals":490,
+    "carb":49
+},{
+    "img":"meal-4.jpg",
+    "title":"Bunless Bison Burger",
+    "desc":"with Spring Succotash & Loaded Cauliflower",
+    "other_desc":"Gluten Free",
+    "cals":540,
+    "carb":36
+},{
+    "img":"meal-5.jpg",
+    "title":"Sausage Baked Penne",
+    "desc":"with Sautéed Zucchini & Spinach",
+    "other_desc":"Gluten Free",
+    "cals":470,
+    "carb":41
+},{
+    "img":"meal-1.jpg",
+    "title":"Steak Peppercorn",
+    "desc":"with Sautéed Carrots & French Green Beans",
+    "other_desc":"Gluten Free",
+    "cals":500,
+    "carb":38
+}];
