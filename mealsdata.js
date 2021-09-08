c07644ed63060ce4c71a11ea718536fdf17785c2
shan-akbar-brainx <shan.akbar@brainxtech.com>
2021-09-08 20:57:34 +0500

    Created checkout form, and some adjustments in previous pages

--- mealsdata.js
+++ mealsdata.js
@@ -1,4 +1,5 @@
 const meals = [{
+    "id":"meal_1",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -6,6 +7,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_2",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -13,6 +15,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_3",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -20,6 +23,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_4",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -27,6 +31,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_5",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -34,6 +39,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_6",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -41,6 +47,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_7",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -48,6 +55,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_8",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -55,6 +63,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_9",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -62,6 +71,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_10",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -69,6 +79,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_11",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -76,6 +87,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_12",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -83,6 +95,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_13",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -90,6 +103,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_14",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -97,6 +111,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_15",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -104,6 +119,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_16",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -111,6 +127,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_17",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -118,6 +135,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_18",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -125,6 +143,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_19",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -132,6 +151,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_20",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -139,6 +159,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_21",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -146,6 +167,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_22",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -153,6 +175,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_23",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -160,6 +183,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_24",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -167,6 +191,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_25",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -174,6 +199,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_26",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -181,6 +207,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_27",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -188,6 +215,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_28",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -195,6 +223,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_29",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -202,6 +231,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_30",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -209,6 +239,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_31",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
@@ -216,6 +247,7 @@ const meals = [{
     "cals":500,
     "carb":38
 },{
+    "id":"meal_32",
     "img":"meal-2.jpg",
     "title":"Homestyle Chicken",
     "desc":"with Masterful Mac & Cheese",
@@ -223,6 +255,7 @@ const meals = [{
     "cals":560,
     "carb":35
 },{
+    "id":"meal_33",
     "img":"meal-3.jpg",
     "title":"Cauliflower Shell Beef Bolognese",
     "desc":"with Nonna’s Soffritto & Italian Cheeses",
@@ -230,6 +263,7 @@ const meals = [{
     "cals":490,
     "carb":49
 },{
+    "id":"meal_34",
     "img":"meal-4.jpg",
     "title":"Bunless Bison Burger",
     "desc":"with Spring Succotash & Loaded Cauliflower",
@@ -237,6 +271,7 @@ const meals = [{
     "cals":540,
     "carb":36
 },{
+    "id":"meal_35",
     "img":"meal-5.jpg",
     "title":"Sausage Baked Penne",
     "desc":"with Sautéed Zucchini & Spinach",
@@ -244,6 +279,7 @@ const meals = [{
     "cals":470,
     "carb":41
 },{
+    "id":"meal_36",
     "img":"meal-1.jpg",
     "title":"Steak Peppercorn",
     "desc":"with Sautéed Carrots & French Green Beans",
